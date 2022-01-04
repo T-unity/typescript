d56ed281379f74b4f10966fc8495656b773a7d76
T-unity <takadanaritoshi@gmail.com>
2022-01-04 22:34:24 +0900

    使用しないコードのスタッシュ

--- src/app.js
+++ src/app.js
@@ -3,20 +3,3 @@ for(let prop in player) {
   if(!player.hasOwnProperty(prop)) continue;
   console.log(prop + ': ' + player[prop]);
 }
-
-
-// class score {}
-
-// class Food {
-//   constructor(public element) {}
-// }
-
-// class Foods {
-//   elements = document.querySelectorAll('.food');
-//   constructor() {
-//     this.elements.forEach(element => {
-//       new Food(element);
-//     })
-//   }
-// }
-// const foods = new Foods();
