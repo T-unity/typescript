3dafbcc7f68256d32e0506586dbda051f456f678
T-unity <takadanaritoshi@gmail.com>
2022-01-06 21:20:35 +0900

    他のクラスから値を取得する

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 
-class score {}
+class Score {
+  get totalScore() {
+    const foods = new Foods();
+    return foods.activeElementsScore.reduce((total, score) => total + score, 0)
+  }
+}
 
 class Food {
   constructor(element) {
