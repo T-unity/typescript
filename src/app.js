59e9ee988cfa641280ad63df0e2749eb0d1c7075
T-unity <takadanaritoshi@gmail.com>
2022-01-06 20:53:28 +0900

    アクセサプロパティを使って配列を作成

--- src/app.js
+++ src/app.js
@@ -12,11 +12,37 @@ class Food {
     // console.log(this.element);
     // this.element.classList.toggle('food--active');
     this.classList.toggle('food--active');
+    // console.log(this.querySelector('.food__score').innerHTML);
   }
 }
 
 class Foods {
   elements = document.querySelectorAll('.food');
+
+  _activeElements = [];
+  _activeElementsScore = [];
+
+  get activeElements() {
+    this._activeElements = [];
+    this.elements.forEach(element => {
+      if (element.classList.contains('food--active')) {
+        this._activeElements.push(element);
+      }
+    })
+    return this._activeElements;
+  }
+
+  get activeElementsScore() {
+    this._activeElementsScore = [];
+    this.activeElements.forEach(element => {
+      const foodScore = element.querySelector('.food__score');
+      if (foodScore) {
+        this._activeElementsScore.push(Number(foodScore.textContent));
+      }
+    })
+    return this._activeElementsScore;
+  }
+
   constructor() {
     this.elements.forEach(element => {
       new Food(element);
